0b7b85b1e3ed4db8b02d99f64d3717be69b51d68
Sindon20 <ginessindonjhamaicamae@gmail.com>
2025-04-04 21:37:58 +0800

    Add files via upload

---
+++ evenNumbers.js
@@ -0,0 +1,10 @@
+console.log("Numbers from 1 to 10:");
+for (let i = 1; i <= 10; i++){
+    console.log(i);
+}
+console.log("\nEven numbers between 1 and 10:");
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
